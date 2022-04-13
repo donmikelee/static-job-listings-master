0c94496d2d3ca12e1b33f5d346b247ab07f3e111
Michał Górny <umpex94@gmail.com>
2022-04-13 18:09:07 +0200

    Create job-ad as list item to writte css code

--- style/main.js
+++ style/main.js
@@ -1,7 +1,17 @@
 import Data from '../data.json';
 
-const img = Data[0].logo;
+const jobList = document.getElementById('job-ad-list');
 
-const app = document.getElementById('app');
+// const appendLi = () => {
+//   for (let data of Data) {
+//     const newItem = document.createElement('li');
 
-app.innerHTML = img;
+//     newItem.innerText = data.jobAdTitle;
+
+//     console.log(newItem);
+
+//     jobList.appendChild(newItem);
+//   }
+// };
+
+// appendLi();
