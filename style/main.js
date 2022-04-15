786eddaa7b96bdd97972a3aae7ddcf72ec02dec9
Michał Górny <umpex94@gmail.com>
2022-04-15 11:40:35 +0200

    Change css files to scss, add root and var files

--- style/main.js
+++ style/main.js
@@ -1,17 +1,3 @@
 import Data from '../data.json';
 
 const jobList = document.getElementById('job-ad-list');
-
-// const appendLi = () => {
-//   for (let data of Data) {
-//     const newItem = document.createElement('li');
-
-//     newItem.innerText = data.jobAdTitle;
-
-//     console.log(newItem);
-
-//     jobList.appendChild(newItem);
-//   }
-// };
-
-// appendLi();
